2fc63f679c0bc88bb26231f5a8a1fcf981636ef0
Chris Price <chris@puppetlabs.com>
2013-11-03 12:49:29 -0800

    Add logging utility and test utility functions

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 ;; Logging
                  [org.clojure/tools.logging "0.2.6"]
+                 [log4j "1.2.17" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]
                  ;; Filesystem utilities
                  [fs "1.1.2"]
                  ;; Configuration file parsing
