5ff35ccea963cee47e5dce6525736161e7f3ea17
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2013-11-22 16:02:42 -0800

    add ':sign-releases false' to project.clj
    we are afraid of GPG signing

--- project.clj
+++ project.clj
@@ -30,5 +30,6 @@
   
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/clojars_jenkins_username
-                                     :password :env/clojars_jenkins_password}]
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]
                         ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]])
