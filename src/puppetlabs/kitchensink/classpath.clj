75e89a8cd90f464fbda9e0099292085b636454f1
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2013-12-20 10:45:54 -0800

    (PE-2410) lift 'add-classpath' out of pomegranate
    https://github.com/cemerick/pomegranate

--- src/puppetlabs/kitchensink/classpath.clj
+++ src/puppetlabs/kitchensink/classpath.clj
@@ -1,6 +1,57 @@
 (ns puppetlabs.kitchensink.classpath
   (:import (java.net URLClassLoader URL))
-  (:require [clojure.java.io :refer [file Coercions]]))
+  (:require [clojure.java.io :refer [file Coercions]]
+            [dynapath.util :as dp])
+  (:refer-clojure :exclude (add-classpath)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Here, we have copied `add-classpath` out of pomegranate
+;;;; (https://github.com/cemerick/pomegranate).
+;;;;
+;;;; We did this because we need to use this function in trapperkeeper,
+;;;; but we did not want to pull pomegranate's rather large dependency tree
+;;;; (we tried excluding as many of its dependencies as possible, but only
+;;;; a small part of its dependency tree was exclude-able).
+;;;;
+;;;; There are no tests for these functions because pomegranate does not
+;;;; contain any tests for them.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- classloader-hierarchy
+  "Returns a seq of classloaders, with the tip of the hierarchy first.
+   Uses the current thread context ClassLoader as the tip ClassLoader
+   if one is not provided."
+  ([] (classloader-hierarchy (.. Thread currentThread getContextClassLoader)))
+  ([tip]
+   (->> tip
+        (iterate #(.getParent %))
+        (take-while boolean))))
+
+(defn- modifiable-classloader?
+  "Returns true iff the given ClassLoader is of a type that satisfies
+   the dynapath.dynamic-classpath/DynamicClasspath protocol, and it can
+   be modified."
+  [cl]
+  (dp/addable-classpath? cl))
+
+(defn add-classpath
+  "A corollary to the (deprecated) `add-classpath` in clojure.core. This implementation
+   requires a java.io.File or String path to a jar file or directory, and will attempt
+   to add that path to the right classloader (with the search rooted at the current
+   thread's context classloader)."
+  ([jar-or-dir classloader]
+   (if-not (dp/add-classpath-url classloader (.toURL (file jar-or-dir)))
+     (throw (IllegalStateException. (str classloader " is not a modifiable classloader")))))
+  ([jar-or-dir]
+   (let [classloaders (classloader-hierarchy)]
+     (if-let [cl (last (filter modifiable-classloader? classloaders))]
+       (add-classpath jar-or-dir cl)
+       (throw (IllegalStateException. (str "Could not find a suitable classloader to modify from "
+                                           classloaders)))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; end of functions copied from pomegranate (see note above)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn jar-or-dir-to-url
   "Given the path to a jar file or a directory, return a `java.net.URL`
