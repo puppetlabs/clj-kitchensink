23000919cfd0c01ebc512a3590bd1302dd00b00b
Deepak Giridharagopal <deepak@puppetlabs.com>
2014-01-06 15:58:57 -0700

    Support loading multiple objs from a PEM
    This is necessary for chained certificate support, as chains are most
    frequently represented as multiple certificates in the same file. Our
    old code would simple parse the first certificate, then quit.
    
    This patch adds new, collection-returning variants of the primitive
    functions that handle all the objects contained in a pem file.

--- src/puppetlabs/kitchensink/ssl.clj
+++ src/puppetlabs/kitchensink/ssl.clj
@@ -4,7 +4,8 @@
            (org.bouncycastle.openssl PEMReader PEMWriter)
            (org.bouncycastle.jce.provider BouncyCastleProvider))
   (:use [clojure.tools.logging :as log]
-        [clojure.java.io :only (reader writer)]))
+        [clojure.java.io :only (reader writer)]
+        [puppetlabs.kitchensink.core :only (enumerate)]))
 
 ;; Need to make sure that the provider is initialized
 (Security/addProvider (BouncyCastleProvider.))
@@ -15,16 +16,28 @@
   (doto (KeyStore/getInstance "JKS")
     (.load nil)))
 
+(defn pem->objs
+  "Given a file path (or any other type supported by clojure's `reader`), reads
+  PEM-encoded objects and returns a collection of objects of the
+  corresponding type from `java.security`."
+  [pem]
+  {:post [(coll? %)]}
+  (let [pemreader (PEMReader. (reader pem))]
+    (loop [objs []]
+      (let [obj (.readObject pemreader)]
+        (if obj
+          (do
+            (log/debug (format "Loaded PEM object of type '%s' from '%s'" (class obj) pem))
+            (recur (conj objs obj)))
+          objs)))))
+
 (defn pem->obj
   "Given a file path (or any other type supported by clojure's `reader`), reads
-  a PEM-encoded object and returns an instance of the corresponding type from
-  `java.security`."
+  a PEM-encoded object and returns an instance of the corresponding
+  type from `java.security`. If the supplied file contains more than
+  one object, only the first is returned."
   [pem]
-  (let [result (-> (reader pem)
-                   (PEMReader.)
-                   (.readObject))]
-    (log/debug (format "Loaded PEM object of type '%s' from '%s'" (class result) pem))
-    result))
+  (first (pem->objs pem)))
 
 (defn obj->pem!
   "Encodes an object in PEM format, and writes it to a file (or other stream).  Arguments:
@@ -41,12 +54,21 @@
       (.writeObject obj)
       (.flush))))
 
-(defn pem->cert
-  "Given the path to a PEM file (or some other object supported by clojure's `reader`),
-  decodes the contents into an instance of `X509Certificate`."
+(defn pem->certs
+  "Given the path to a PEM file (or some other object supported by
+  clojure's `reader`), decodes the contents into an collection of
+  `X509Certificate` instances."
   [pem]
-  {:post [(instance? X509Certificate %)]}
-  (pem->obj pem))
+  {:post [(every? (fn [x] (instance? X509Certificate x)) %)]}
+  (pem->objs pem))
+
+(defn pem->cert
+  "Given the path to a PEM file (or some other object supported by
+  clojure's `reader`), decodes the contents into an instance of
+  `X509Certificate`. If the supplied file contains more than one
+  certificate, only the first is returned."
+  [pem]
+  (first (pem->objs pem)))
 
 (defn pem->private-key
   "Given the path to a PEM file (or some other object supported by clojure's `reader`),
@@ -101,6 +123,20 @@
   `pem-cert`: the path to a PEM file containing the certificate"
   [keystore alias pem-cert]
   (assoc-cert! keystore alias (pem->cert pem-cert)))
+
+(defn assoc-certs-from-file!
+  "Add all certificates from a PEM file to a keystore.  Arguments:
+
+  `keystore`: the `KeyStore` to add certificates to
+  `prefix`:   an alias to associate with the certificates. each
+              certificate will have a numeric index appended to
+              its alias (starting with '-0'
+  `pem`:      the path to a PEM file containing the certificate"
+  [keystore prefix pem]
+  (doseq [[i cert] (enumerate (pem->certs pem))
+          :let [alias (format "%s-%d" prefix i)]]
+    (assoc-cert! keystore alias cert))
+  keystore)
 
 (defn assoc-private-key!
   "Add a private key to a keystore.  Arguments:
