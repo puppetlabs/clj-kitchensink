a8297ed9d963b9b31ec8cbb7b8e01cb9599bb698
Jenkins User <jenkins@debian-7-x64.acctest.dc1.puppetlabs.net>
2014-02-18 15:13:28 -0800

    lein-release plugin: preparing 0.5.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/kitchensink "0.5.2-SNAPSHOT"
+(defproject puppetlabs/kitchensink "0.5.2"
   :description "Clojure utility functions"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
