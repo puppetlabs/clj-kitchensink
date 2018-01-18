00495b71b6d04c04aeaa3466c9cae83db8fdbb79
Patrick Carlisle <patrick@puppetlabs.com>
2018-01-18 14:35:52 -0800

    (maint) Clean up with-additional-classpath-entries test
    The implementation of this test was not ideal, and made worse by Java 9 changes
    to classloaders. Instead of querying URLs, just add a resource to a directory
    that's not normally in the class path and test that we can resolve it when it's
    added and that we can't after it gets cleaned up.

--- test/puppetlabs/kitchensink/classpath_test.clj
+++ test/puppetlabs/kitchensink/classpath_test.clj
@@ -1,26 +1,17 @@
 (ns puppetlabs.kitchensink.classpath-test
   (:require [clojure.test :refer :all]
-            [dynapath.dynamic-classpath :refer [classpath-urls]]
-            [puppetlabs.kitchensink.classpath :refer [add-classpath
-                                                      with-additional-classpath-entries]])
-  (:import (java.net URL))
-  (:refer-clojure :exclude (add-classpath)))
+            [puppetlabs.kitchensink.classpath :refer [with-additional-classpath-entries]])
+  (:import (java.net URL)))
 
 (deftest with-additional-classpath-entries-test
-  (let [paths ["/foo" "/bar"]
-        get-urls #(into #{}
-                        (classpath-urls (.getContextClassLoader (Thread/currentThread))))]
-    ;; Called for side effect of ensuring there's a modifiable classloader
-    ;; without needing to make that function public
-    (add-classpath "file:/nil")
-
+  (let [paths ["classpath-test"]
+        get-resource #(-> (Thread/currentThread)
+                          .getContextClassLoader
+                          (.getResource "does-not-exist-anywhere-else"))]
     (with-additional-classpath-entries
       paths
-      (testing "classloader now includes the new paths"
-        (let [urls (get-urls)]
-          (is (contains? urls (URL. "file:/foo")))
-          (is (contains? urls (URL. "file:/bar"))))))
-    (testing "classloader has been restored to its previous state"
-      (let [urls (get-urls)]
-        (is (not (contains? urls (URL. "file:/foo"))))
-        (is (not (contains? urls (URL. "file:/bar"))))))))
+      (testing "classloader now includes the new path"
+        (is (get-resource))))
+
+    (testing "classloader no longer includes the new path"
+      (is (not (get-resource))))))
