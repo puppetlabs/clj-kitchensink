a2e0020063d204f2ca7644235e97434f964e67d7
Jenkins CI <ci@puppet.com>
2023-11-09 23:18:05 +0000

    Version 3.2.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/kitchensink "3.2.4"
+(defproject puppetlabs/kitchensink "3.2.5-SNAPSHOT"
   :description "Clojure utility functions"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
